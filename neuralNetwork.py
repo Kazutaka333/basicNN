1dd034544a6a1bf5701a2a2fcbc605048b08ad42
Kazutaka Homma <special18fender@gmail.com>
2017-03-14 21:41:45 -0700

    added neuralNework class

---
+++ neuralNetwork.py
@@ -0,0 +1,32 @@
+# neural network class definition
+class neuralNetwork:
+    
+    # initialize the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        
+        # learning rate
+        self.lr = learningrate
+        
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        # basic version 
+        # self.wih = (numpy.random.rand(self.hnodes, self.inodes) - 0.5)
+        # self.who = (numpy.random.rand(self.onodes, self.hnodes) - 0.5)
+        # adjusted version
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        pass
+    
+    # train the neural network
+    def train():
+        pass
+    
+    # query the neural network
+    def query():
+        pass
