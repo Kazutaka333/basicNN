3a661341f6eeb2b6d95af7158f276a941b78a012
Kazutaka Homma <special18fender@gmail.com>
2017-04-16 15:59:18 -0700

    remove other than neuralNetwork class

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,4 +1,10 @@
+import numpy
+# scipy.special for the sigmoid function expit()
+import scipy.special
+import matplotlib.pyplot
+
 # neural network class definition
+
 class neuralNetwork:
     
     # initialize the neural network
@@ -12,15 +18,20 @@ class neuralNetwork:
         self.lr = learningrate
         
         # link weight matrices, wih and who
-        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # weights inside the arrays are w_i_j, where link is from node i to node 
+        # j in the next layer
         # w11 w21
         # w12 w22 etc
         # basic version 
         # self.wih = (numpy.random.rand(self.hnodes, self.inodes) - 0.5)
         # self.who = (numpy.random.rand(self.onodes, self.hnodes) - 0.5)
         # adjusted version
-        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
-        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        self.wih = numpy.random.normal(0.0, 
+                                       pow(self.hnodes, -0.5),
+                                       (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, 
+                                       pow(self.onodes, -0.5), 
+                                       (self.onodes, self.hnodes))
         pass
     
     # train the neural network
@@ -42,18 +53,21 @@ class neuralNetwork:
 	# error is the (target - actual)
         output_errors = targets - final_outputs
         
-        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+        # hidden layer error is the output_errors, split by weights, recombined 
+        # at hidden nodes
         hidden_errors = numpy.dot(self.who.T, output_errors)
         
         # update the weights for the links between the hidden and output layers
         # * is to multiply each item at the same position (j,k) in two matrices
         # numpy.dot() is to do dot product with two matrices
-        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs),
-                                         numpy.transpose(hidden_outputs))
+        self.who += self.lr * numpy.dot(
+                (output_errors * final_outputs * (1.0 - final_outputs)),
+                numpy.transpose(hidden_outputs))
         
         # update the weights for the links between the input and hidden layers
-        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs),
-                                         numpy.transpose(inputs))
+        self.wih += self.lr * numpy.dot(
+                (hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                numpy.transpose(inputs))
         pass
     
     # query the neural network
@@ -73,14 +87,22 @@ class neuralNetwork:
         
         return final_outputs 
 
+    def getWIH(self):
+        return self.wih
+    def getWHO(self):
+        return self.who
+    def setWIH(self, wih):
+        self.wih = wih
+    def getWHO(self, who):
+        self.who = who
 
 # number of input, hidden and output nodes
 input_nodes = 784
-hidden_nodes = 100
+hidden_nodes = 200
 output_nodes = 10
 
 # learning raet is 0.3
-learning_rate = 0.3
+learning_rate = 0.15
 
 # create instance of neural network
 n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
@@ -98,7 +120,8 @@ for record in training_data_list:
     all_values = record.split(',')
     # scale and shift the inputs
     inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    # create the target output values (all 0.01, except the desired label which is 0.99)
+    # create the target output values (all 0.01, except the desired label which 
+    # is 0.99)
     targets = numpy.zeros(output_nodes) + 0.01
     # all_values[0] is the target label for this record
     targets[int(all_values[0])] = 0.99
@@ -113,5 +136,5 @@ training_data_file.close()
 all_values = training_data_list[0].split(',')
 
 print(all_values[0])
-
+    
 n.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01)
