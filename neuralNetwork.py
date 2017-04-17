e035910805af4eb916986b7bc97b776f17afc4f1
Kazutaka Homma <special18fender@gmail.com>
2017-04-16 17:28:33 -0700

    remove non class part

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -97,48 +97,6 @@ class neuralNetwork:
         return self.who
     def setWIH(self, wih):
         self.wih = wih
-    def getWHO(self, who):
+    def setWHO(self, who):
         self.who = who
 
-# number of input, hidden and output nodes
-input_nodes = 784
-hidden_nodes = 200
-output_nodes = 10
-
-# learning raet is 0.3
-learning_rate = 0.15
-
-# create instance of neural network
-n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
-
-
-training_data_file = open("MNIST/mnist_train_100.csv", 'r')
-training_data_list = training_data_file.readlines()
-training_data_file.close()
-
-# train the neural network
-
-# go through all records in the training data set 
-for record in training_data_list:
-    # split the record by the ',' commas
-    all_values = record.split(',')
-    # scale and shift the inputs
-    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    # create the target output values (all 0.01, except the desired label which 
-    # is 0.99)
-    targets = numpy.zeros(output_nodes) + 0.01
-    # all_values[0] is the target label for this record
-    targets[int(all_values[0])] = 0.99
-    n.train(inputs, targets)
-    pass
- 
-
-training_data_file = open("MNIST/mnist_test_10.csv", 'r')
-training_data_list = training_data_file.readlines()
-training_data_file.close()
-
-all_values = training_data_list[0].split(',')
-
-print(all_values[0])
-    
-n.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01)
